f174c0a74d676df91c670ebf5da21c2e34ab50cd
Eren Çataltepe <erencataltepe@gmail.com>
2021-06-22 17:20:25 +0300

    Add modal screen.

--- script.js
+++ script.js
@@ -46,4 +46,36 @@ function renderBooks(books) {
   });
 }
 
+//const newBookButton = document.querySelector(".add-new-book-button");
+
+//newBookButton.addEventListener("click", () => {});
+
+// Get the modal
+const modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+const newBookButton = document.querySelector(".add-new-book-button");
+
+// Get the <span> element that closes the modal
+const span = document.getElementsByClassName("close")[0];
+
+const addBookButton 
+
+// When the user clicks on the button, open the modal
+newBookButton.onclick = function () {
+  modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+
 renderBooks(books);
