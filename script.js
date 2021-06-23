cbf5f1f96660fa74587d99ad0cef6afad43b8a7c
Eren Çataltepe <erencataltepe@gmail.com>
2021-06-23 10:50:41 +0300

    Add remove book function.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let library = [];
 let currentBookId = 0;
 
+//Book constructor function
 function Book(title, author, pageNumber, readStatus, id) {
   this.title = title;
   this.author = author;
@@ -9,13 +10,22 @@ function Book(title, author, pageNumber, readStatus, id) {
   this.id = id;
 }
 
+//Create bookcard html for a book object
 function createBookCard(book) {
   const bookSection = document.querySelector(".book-section");
-  const bookCard = document.createElement("article");
+  const bookCard = document.createElement("div");
   const bookTitle = document.createElement("h2");
   const authorInfo = document.createElement("div");
   const pageInfo = document.createElement("div");
   const readInfo = document.createElement("div");
+  const removeButton = document.createElement("button");
+
+  removeButton.classList.add("remove-buttons");
+  removeButton.textContent = "Remove Book";
+  removeButton.setAttribute("data-id", book.id);
+  removeButton.addEventListener("click", (e) => {
+    removeBook(e.target.dataset.id);
+  });
 
   bookTitle.innerText = book.title;
   authorInfo.innerText = book.author;
@@ -23,32 +33,56 @@ function createBookCard(book) {
   readInfo.innerText = book.readStatus;
 
   bookCard.setAttribute("data-id", book.id);
+  bookCard.classList.add("book-cards");
   bookCard.appendChild(bookTitle);
   bookCard.appendChild(authorInfo);
   bookCard.appendChild(pageInfo);
   bookCard.appendChild(readInfo);
+  bookCard.appendChild(removeButton);
   bookSection.appendChild(bookCard);
 }
 
+//Render all books in the library array to the page
 function renderBooks(library) {
   library.forEach((book) => {
     createBookCard(book);
   });
 }
 
+//Add book to the library array
 function addBookToLibrary(book) {
   library.push(book);
 }
 
+//Clear modal inputs values after submit new book.
 function clearInputs() {
   document.getElementById("new-title").value = "";
   document.getElementById("new-author").value = "";
   document.getElementById("new-pages").value = "";
 }
 
-//const newBookButton = document.querySelector(".add-new-book-button");
+//Remove Book
 
-//newBookButton.addEventListener("click", () => {});
+function removeBook(removeId) {
+  removeBookFromLibrary(removeId);
+  removeBookFromLayout(removeId);
+}
+
+//Remove book from library
+function removeBookFromLibrary(id) {
+  const tempLibrary = library.filter((book) => book.id != id);
+  library = tempLibrary;
+}
+
+//Remove book from layout
+function removeBookFromLayout(id) {
+  const bookCards = document.querySelectorAll(".book-cards");
+  bookCards.forEach((bookCard) => {
+    if (bookCard.dataset.id == id) {
+      bookCard.remove();
+    }
+  });
+}
 
 // Get the modal
 const modal = document.getElementById("myModal");
@@ -102,5 +136,3 @@ window.onclick = function (event) {
     modal.style.display = "none";
   }
 };
-
-renderBooks(library);
